adeb990ee060ac676db839c3dd9d937dd0c637ef
Makchcjp650 <99318126+Makchcjp650@users.noreply.github.com>
2022-04-02 22:05:34 +0300

    lab.rob_5.8-3
    lab.rob_5.8-3

--- lab.rob_5.8-3/src/com/company/Main.java
+++ lab.rob_5.8-3/src/com/company/Main.java
@@ -3,29 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        int floors = 2;
-        String floor;
-        if (floors == 1){
-            floor = "1";
-        }
-        if (floors == 2){
-            System.out.println("not working");
-        }
-        if (floors == 3){
-        }
-        if (floors == 4){
-        }
-        if (floors == 5){
-        }
-        if (floors == 6){
-        }
-        if (floors == 7){
-        }
-        if (floors == 8){
-        }
-        if (floors == 9){
-        }
-        System.out.println("your floor is " + floors + ".");
+        int floor = 2;
+        String down = " down ";
+        String down1 = " down ";
+        String up = " up ";
+        if ((0 < floor) & (floor < 10)) {
+            if (down.equals(up)){
+                if (floor == 2) {
+                    System.out.println("you up on 3 floor");
+                } else System.out.println("you up on " + floor + " floor");
+            } else if (down.equals(down1)) {
+                if (floor == 2){
+                    System.out.println("you down on 1 floor");
+                } else System.out.println("you down on " + floor + " floor");
+            }else System.out.print("unknown line");
+        }else System.out.println("unknown floor");
     }
 }

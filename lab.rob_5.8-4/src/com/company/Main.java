0a77f8c42cb81537ea6830ae787dac61a43378ae
Makchcjp650 <99318126+Makchcjp650@users.noreply.github.com>
2022-04-02 22:18:22 +0300

    lab.rob_5.8-4
    lab.rob_5.8-4

--- lab.rob_5.8-4/src/com/company/Main.java
+++ lab.rob_5.8-4/src/com/company/Main.java
@@ -3,54 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        switch (1) {
-            case 1:
-                System.out.println("Погодитись");
-                switch (2) {
-                    case 0:
-                        System.out.println("Так");
-                        break;
-                        case 1:
-                            System.out.println("OK");
-                            break;
-                            case 2:
-                                System.out.println("Yes");
-                                break;
-                                case 3:
-                                    System.out.println("Y");
-                                    break;
-                                    case 4:
-                                        System.out.println("+");
-                                        break;
-                                        case 5:
-                                            System.out.println("Ok");
-                                            break;
+        String a = "-";
+        switch (a) {
+            case "Так":
+            case "Yes":
+            case "OK":
+            case "Y":
+            case "Ok":
+            case "+":
+                System.out.println("Я погоджуюсь ");
+                break;
+            case "Ні":
+            case "NO":
+            case "N":
+            case "No":
+            case "-":
+                System.out.println("Я відмовляюсь ");
+                break;
+            default: System.out.println("Ви ввели не існуючі дані");
         }
-
-        }
-        switch (1) {
-            case 1:
-                System.out.println("Відмовитись");
-                switch (1) {
-                    case 0:
-                        System.out.println("Ні");
-                        break;
-                    case 1:
-                        System.out.println("NO");
-                        break;
-                    case 2:
-                        System.out.println("N");
-                        break;
-                    case 3:
-                        System.out.println("-");
-                        break;
-                    case 4:
-                        System.out.println("No");
-                        break;
-                }
-
-        }
-
     }
 }

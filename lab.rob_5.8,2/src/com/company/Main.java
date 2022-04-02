ccb874c5a5a753f0d0bf0896cf05d7011d13312e
Makchcjp650 <99318126+Makchcjp650@users.noreply.github.com>
2022-04-02 21:41:34 +0300

    lab.rob_5.8,2
    lab.rob_5.8,2

--- lab.rob_5.8,2/src/com/company/Main.java
+++ lab.rob_5.8,2/src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        for (int n=1, m=10; n<=10; n++) {
-            System.out.println(m);
-            break;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a = n % 10;
+        int b = (n % 100 - a) / 10;
+        n = ( n - a - b * 10) / 100;
+        int z = Math.max( n , b);
+        System.out.println(Math.max( z , b ));
         }
     }
-}
+
